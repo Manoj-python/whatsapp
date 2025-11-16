c6b2c0955a50cfac7c7f6ef06cfc88abb878d3ee
Manoj-python <manojpythonfullstackdeveloper@gmail.com>
2025-11-16 19:09:35 +0530

    Fix comment formatting in settings.py

--- whatsapp_sender/settings.py
+++ whatsapp_sender/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 
 # ---------------------------------------------------------------------
 # MIDDLEWARE
-6# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
